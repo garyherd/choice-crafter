e46006f009dbadd9952e507dc212d4c22230337f
Gary Herd <garyherd5@gmail.com>
2017-05-29 19:53:30 -0500

    Update tradeoff page

--- src/components/tradeoffs.js
+++ src/components/tradeoffs.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
-//import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Panel } from 'react-bootstrap';
 
 
 
 class TradeOffs extends Component {
     render() {
         return (
-            <div>
+            /*<div>
                 This will be the TradeOffs tab
-            </div>
+            </div>*/
+            <section>
+                <Row>
+                    <Col sm={6}>
+                        <Panel>
+                            <h5>To make and Even Swap trade-off:</h5>
+                            <ol>
+                                <li>choose three cells from the consequences table</li>
+                                <li>when ready, press the Even Swap button below</li>
+                            </ol>
+                        </Panel>
+                    </Col>
+                    <Col sm={6}>This will be the tradeoff status/action section</Col>
+                </Row>
+                <Row>
+                    <Col xs={12}>This will be the consequences grid</Col>
+                </Row>
+            </section>
         );
     }
 }
