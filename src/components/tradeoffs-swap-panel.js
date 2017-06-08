86286118fea3112c849c2c5d934e226f36e4ea5c
Gary Herd <garyherd5@gmail.com>
2017-06-08 15:17:19 -0500

    Add selectors to swap pane

--- src/components/tradeoffs-swap-panel.js
+++ src/components/tradeoffs-swap-panel.js
@@ -6,12 +6,19 @@ class SwapPanel extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {currentSelectedAlternative: 'Select', currentSelectedSwapChoice: 'Select'};
+    this.state = {
+      currentSelectedAlternative: 'Select', 
+      currentSelectedSwapChoice: 'Select',
+    };
     this.handleSelectedAlternativeChange = this.handleSelectedAlternativeChange.bind(this);
+    this.handleSelectedSwapChoiceChange = this.handleSelectedSwapChoiceChange.bind(this);
   }
 
   handleSelectedAlternativeChange(event) {
-    this.setState({ currentSelectedAlternative: event.target.value });
+    this.setState(
+      {
+        currentSelectedAlternative: event.target.value,
+      });
   }
 
   handleSelectedSwapChoiceChange(event) {
@@ -24,10 +31,15 @@ class SwapPanel extends Component {
       <option value={alternative.title} key={alternative.id}>{alternative.title}</option>
     );
 
+    //TODO: filter out 'other' objective, so user can't select
+    //same objectives for swap
     const objectivesSelectOptions = this.props.objectives.map(objective =>
       <option value={objective.title} key={objective.id}>{objective.title}</option>
     );
 
+    let score = this.props.getConsequence(this.state.currentSelectedSwapChoice, this.state.currentSelectedAlternative).score;
+
+    console.log(score);
     return (
       <Panel>
         <FormGroup controlId="formControlsSelect">
@@ -42,18 +54,23 @@ class SwapPanel extends Component {
         </FormGroup>
 
         <p>
-          <strong>The change in&nbsp;</strong>
-          <FormControl
-            componentClass="select"
-            bsSize="small"
-            value={this.state.currentSelectedAlternative}
-            onChange={this.handleSelectedAlternativeChange}>
+          <strong>The change in:&nbsp;</strong>
+          <select
+            value={this.state.currentSelectedSwapChoice}
+            onChange={this.handleSelectedSwapChoiceChange}>
             <option value="Select">Select</option>
             {objectivesSelectOptions}
-          </FormControl>
+          </select>
         </p>
-        <p><strong>From&nbsp;</strong>1700<strong>&nbsp;To&nbsp;</strong><input type="text" name="newSwapFront" value={1500} /></p>
-        <p><strong>can be compensated for by a change in&nbsp;</strong>Benefits</p>
+        <p>
+          <strong>From:&nbsp;</strong>
+          {score}
+          <strong>&nbsp;To&nbsp;</strong>
+          <input type="text" name="newSwapFront" value={1500} /></p>
+        <p>
+          <strong>can be compensated for by a change in:&nbsp;</strong>
+          Benefits
+        </p>
         <p><strong>From&nbsp;</strong>A<strong>&nbsp;To&nbsp;</strong><input type="text" name="newSwapBack" value="B" /></p>
         <ButtonToolbar>
           <Button bsStyle="primary" type="button">OK</Button>
